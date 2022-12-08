4a851c13ea0def50afe42f5f2c02c2acf7994633
diego-rivera-e <diegoriveraespinoza@gmail.com>
2022-12-08 19:42:13 -0300

    Desafio funciones JS

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+ /*function pintar(){
+            ele = document.getElementById("ele1")
+            ele.style.backgroundColor ='yellow'
+        }
+        ele = document.getElementById("ele1");
+        ele.addEventListener("click", pintar);
+
+        ele = document.getElementById("ele1");
+        ele.addEventListener("click", function(){
+            ele.style.backgroundColor ="yelow";
+        })
+        */
+        pintar = (ele, color = 'green') => ele.style.backgroundColor = color;
+        ele = document.getElementById("ele1");
+        ele.addEventListener("click", () => pintar(ele,'red'))
